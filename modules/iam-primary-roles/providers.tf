f7b84e9f9e023a90c3c1dbb97709ed5d50d620c2
Mathieu <mathieu.0598@gmail.com>
2021-02-22 17:22:52 +0100

    Revert changes on iam-primary-roles

--- modules/iam-primary-roles/providers.tf
+++ modules/iam-primary-roles/providers.tf
@@ -1,22 +1,22 @@
 provider "aws" {
   region = var.region
 
-  # dynamic "assume_role" {
-  #   for_each = module.iam_roles.org_role_arn != null ? [true] : []
-  #   content {
-  #     # `terraform import` will not use data from a data source,
-  #     # so on import we have to explicitly specify the role
-  #     role_arn = coalesce(var.import_role_arn, module.iam_roles.org_role_arn)
-  #   }
-  # }
+  dynamic "assume_role" {
+    for_each = module.iam_roles.org_role_arn != null ? [true] : []
+    content {
+      # `terraform import` will not use data from a data source,
+      # so on import we have to explicitly specify the role
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.org_role_arn)
+    }
+  }
 }
 
-# module "iam_roles" {
-#   source      = "../account-map/modules/iam-roles"
-#   stage       = var.stage
-#   assume_role = false
-#   region      = var.region
-# }
+module "iam_roles" {
+  source      = "../account-map/modules/iam-roles"
+  stage       = var.stage
+  tfstate_assume_role = false
+  region      = var.region
+}
 
 variable "import_role_arn" {
   type        = string
