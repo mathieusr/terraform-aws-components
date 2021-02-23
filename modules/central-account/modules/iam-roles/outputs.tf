da49704a5a1ceb0ba320dd0a99b4c41fe81534c2
Mathieu <mathieu.0598@gmail.com>
2021-02-23 12:07:03 +0100

    Test element on iam roles

--- modules/central-account/modules/iam-roles/outputs.tf
+++ modules/central-account/modules/iam-roles/outputs.tf
@@ -1,7 +1,10 @@
 output "terraform_role_arn" {
-  value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? (
-  "") : data.terraform_remote_state.account_map.outputs.terraform_roles[module.this.stage]
+  value = ""
 }
+# output "terraform_role_arn" {
+#   value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? (
+#   "") : data.terraform_remote_state.account_map.outputs.terraform_roles[module.this.stage]
+# }
 
 output "org_role_arn" {
   value = null
@@ -14,14 +17,22 @@ output "org_role_arn" {
 #   )
 # }
 
-output "dns_terraform_role_arn" {
-  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.dns_account_stage_name]
+# output "dns_terraform_role_arn" {
+#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.dns_account_stage_name]
+# }
+
+# output "audit_terraform_role_arn" {
+#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
+# }
+
+# output "identity_terraform_role_arn" {
+#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.identity_account_stage_name]
+# }
+
+output "test_bucket" {
+  value = local.tfstate_bucket
 }
 
-output "audit_terraform_role_arn" {
-  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
-}
-
-output "identity_terraform_role_arn" {
-  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.identity_account_stage_name]
+output "tfstate_dynamodb_table" {
+  value = local.tfstate_bucket
 }
--- modules/central-account/modules/iam-roles/tfstate.tf
+++ modules/central-account/modules/iam-roles/tfstate.tf
@@ -1,15 +1,15 @@
-data "terraform_remote_state" "account_map" {
-  backend   = "s3"
-  workspace = "${var.tfstate_role_environment_name}-${var.tfstate_role_stage_name}"
+# data "terraform_remote_state" "account_map" {
+#   backend   = "s3"
+#   workspace = "${var.tfstate_role_environment_name}-${var.tfstate_role_stage_name}"
 
-  config = {
-    encrypt              = true
-    bucket               = local.tfstate_bucket
-    workspace_key_prefix = "account-map"
-    key                  = "terraform.tfstate"
-    dynamodb_table       = local.tfstate_dynamodb_table
-    region               = var.region
-    role_arn             = local.tfstate_access_role_arn
-    acl                  = "bucket-owner-full-control"
-  }
-}
+#   config = {
+#     encrypt              = true
+#     bucket               = local.tfstate_bucket
+#     workspace_key_prefix = "account-map"
+#     key                  = "terraform.tfstate"
+#     dynamodb_table       = local.tfstate_dynamodb_table
+#     region               = var.region
+#     role_arn             = local.tfstate_access_role_arn
+#     acl                  = "bucket-owner-full-control"
+#   }
+# }
