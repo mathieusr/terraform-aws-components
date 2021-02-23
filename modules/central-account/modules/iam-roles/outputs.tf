40e45137dc52553ee0cbd7bce6b2e64c94abb497
Mathieu <mathieu.0598@gmail.com>
2021-02-23 14:44:19 +0100

    Test element on iam roles

--- modules/central-account/modules/iam-roles/outputs.tf
+++ modules/central-account/modules/iam-roles/outputs.tf
@@ -1,10 +1,7 @@
 output "terraform_role_arn" {
-  value = ""
+  value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? (
+  "") : data.terraform_remote_state.account_map.outputs.terraform_roles[module.this.stage]
 }
-# output "terraform_role_arn" {
-#   value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? (
-#   "") : data.terraform_remote_state.account_map.outputs.terraform_roles[module.this.stage]
-# }
 
 output "org_role_arn" {
   value = null
@@ -17,22 +14,18 @@ output "org_role_arn" {
 #   )
 # }
 
-# output "dns_terraform_role_arn" {
-#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.dns_account_stage_name]
-# }
-
-# output "audit_terraform_role_arn" {
-#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
-# }
-
-# output "identity_terraform_role_arn" {
-#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.identity_account_stage_name]
-# }
-
-output "test_bucket" {
-  value = local.tfstate_bucket
+output "dns_terraform_role_arn" {
+  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.dns_account_stage_name]
 }
 
-output "tfstate_dynamodb_table" {
+output "audit_terraform_role_arn" {
+  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
+}
+
+output "identity_terraform_role_arn" {
+  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.identity_account_stage_name]
+}
+
+output "test_bucket" {
   value = local.tfstate_bucket
 }
