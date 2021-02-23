b98b6338d6bdd90395140221f14dd1937353a4e3
Mathieu <mathieu.0598@gmail.com>
2021-02-23 17:56:03 +0100

    Update dns-primary conf

--- modules/dns-primary/providers.tf
+++ modules/dns-primary/providers.tf
@@ -12,6 +12,9 @@ module "iam_roles" {
   source = "../central-account/modules/iam-roles"
   stage  = var.stage
   region = var.region
+  tfstate_bucket_environment_name = var.tfstate_bucket_environment_name
+  context     = module.this.context
+  tfstate_account_id = var.tfstate_account_id
 }
 
 variable "import_role_arn" {
