707bc574403793c3c403da5ab0ca8129ddf23127
Mathieu <mathieu.0598@gmail.com>
2021-02-23 14:46:58 +0100

    Final correction on iam-roles

--- modules/central-account/modules/iam-roles/outputs.tf
+++ modules/central-account/modules/iam-roles/outputs.tf
@@ -18,9 +18,9 @@ output "dns_terraform_role_arn" {
   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.dns_account_stage_name]
 }
 
-output "audit_terraform_role_arn" {
-  value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
-}
+# output "audit_terraform_role_arn" {
+#   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.audit_account_stage_name]
+# }
 
 output "identity_terraform_role_arn" {
   value = data.terraform_remote_state.account_map.outputs.terraform_roles[data.terraform_remote_state.account_map.outputs.identity_account_stage_name]
