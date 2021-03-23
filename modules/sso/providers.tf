06ad240dc3d3da9d3d49ac3036cca99f56a49e32
Mathieu <mathieu.0598@gmail.com>
2021-03-23 16:56:16 +0100

    Update sso to use central account modules and adapt param

--- modules/sso/providers.tf
+++ modules/sso/providers.tf
@@ -12,10 +12,13 @@ provider "aws" {
 }
 
 module "iam_roles" {
-  source      = "../account-map/modules/iam-roles"
+  source      = "../central-account/modules/iam-roles"
   stage       = var.stage
   assume_role = false
   region      = var.region
+  tfstate_bucket_environment_name = var.tfstate_bucket_environment_name
+  context     = module.this.context
+  tfstate_account_id = var.tfstate_account_id
 }
 
 variable "import_role_arn" {
