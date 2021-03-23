492f5ede985345be532cdead2cc497688f545ef3
Mathieu <mathieu.0598@gmail.com>
2021-03-23 17:51:59 +0100

    Update iam roles to match cip context

--- modules/central-account/modules/iam-roles/outputs.tf
+++ modules/central-account/modules/iam-roles/outputs.tf
@@ -3,8 +3,15 @@ output "terraform_role_arn" {
   "") : data.terraform_remote_state.account_map.outputs.terraform_roles[module.this.stage]
 }
 
+# output "org_role_arn" {
+#   value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? null : format(
+#     "arn:aws:iam::%s:role/CipProdAccessRole",
+#     data.terraform_remote_state.account_map.outputs.full_account_map[module.this.stage]
+#   )
+# }
+
 output "org_role_arn" {
-  value = module.this.stage == data.terraform_remote_state.account_map.outputs.root_account_stage_name ? null : format(
+  value =  data.terraform_remote_state.account_map.outputs.full_account_map[module.this.stage] == data.terraform_remote_state.account_map.outputs.full_account_map[data.terraform_remote_state.account_map.outputs.root_account_stage_name] ? null : format(
     "arn:aws:iam::%s:role/CipProdAccessRole",
     data.terraform_remote_state.account_map.outputs.full_account_map[module.this.stage]
   )
