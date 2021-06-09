8371238097cababcb980a045edf9e328839aa720
Mathieu <mathieu.0598@gmail.com>
2021-06-09 11:19:16 +0200

    Correct  add exta env to customize node group user data

--- modules/eks/modules/node_group_by_region/variables.tf
+++ modules/eks/modules/node_group_by_region/variables.tf
@@ -28,5 +28,6 @@ variable "cluster_context" {
     module_depends_on         = any
     public_subnet             = bool
     existing_workers_role_policy_arns = list(string)
+    before_cluster_joining_userdata = string
   })
 }
